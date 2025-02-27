dab5406fd4130cf513cc5bf39c07013ad7b9db97
Yerassyl <fake_era@mail.ru>
2025-02-27 18:16:35 +0500

    fix change site in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://azamat.ai',
+  // site: 'https://azamat.ai',
+  site: 'https://khazidhea.github.io',
   base: '/',
   integrations: [],
   output: 'static',
